af51247d66bf7e73047c28c30de3d04fc111170a
Hena-yaris <meraey21@gmail.com>
2024-02-27 21:42:01 -0800

    product detail

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import LayOut from '../../Components/LayOut/LayOut'
-
+import React, { useEffect, useState } from "react";
+import LayOut from "../../Components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoints";
+import ProductCard from "../../Components/Product/ProductCard";
 function ProductDetail() {
- 
+  const [product, setProduct] = useState({});
+
+  const { productId } = useParams();
+  useEffect(() => {
+    axios
+      .get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+       
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <LayOut>
       
-      <h1>Product details</h1>
+        <ProductCard
+          product={product}
+          // flex={true}
+          // renderDesc={true}
+          // renderAdd={true}
+        />
+      
     </LayOut>
   );
 }
 
-export default ProductDetail
+export default ProductDetail;
