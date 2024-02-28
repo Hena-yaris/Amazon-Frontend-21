1f300716efaa6c918ff1860bfd7d4194cbda9236
Hena-yaris <meraey21@gmail.com>
2024-02-28 09:58:04 -0800

    second cart functionality

--- src/utility/reducer.js
+++ src/utility/reducer.js
@@ -8,14 +8,27 @@ export const initialState = {
 export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
+      // * check if the item exists
+      const existingItem = state.basket.find(
+        (item) => item.id === action.item.id
+      );
+      if (!existingItem) {
+        return {
+          //yh malet statachnnn kipe adrgo be action amakagnnet add yemideregewn item add eyaderegn new.
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        const updatedBasket = state.basket.map((item) => {
+          return item.id === action.item.id
+            ? { ...item, amount: item.amount + 1 }
+            : item;
+        });
 
-    return {
-
-      ...state,
-      basket:[...state.basket,action.basket]
-    }
-
-    default:
-      return state;
+        return {
+          ...state,
+          basket: updatedBasket,
+        };
+      }
   }
 };
