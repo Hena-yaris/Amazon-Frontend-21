97f88aa955ba55f1a8457ac1693a6dc22c7ed712
Hena-yaris <meraey21@gmail.com>
2024-02-27 18:54:22 -0800

    product

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import CarouselEffect from './Components/Carousel/CarouselEffect'
 import Category from './Components/Category/Category'
 import Header from './Components/Header/Header'
+import Product from './Components/Product/Product'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <CarouselEffect/>
     <Category/>
+    <Product/>
     
     </>
   )
