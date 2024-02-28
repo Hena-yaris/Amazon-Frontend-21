2999621eeead30a02e97785ae612e791dd2835ee
Hena-yaris <meraey21@gmail.com>
2024-02-28 09:47:58 -0800

    first cart

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -4,7 +4,47 @@ import LayOut from "../../Components/LayOut/LayOut";
 const Cart = () => {
   return (
     <LayOut>
-      <div>Cart</div>
+      <section className={classes.container}>
+        <div className={classes.cart__container}>
+          <h2>Hello</h2>
+          <h3>Your shopping basket </h3>
+          <hr />
+          {basket?.length == 0 ? (
+            <p>Opps ! No item in your cart</p>
+          ) : (
+            basket?.map((item, i) => {
+              return (
+                <section className={classes.cart_product}>
+                  <ProductCard
+                    key={i}
+                    product={item}
+                    renderDesc={true}
+                    renderAdd={false}
+                    flex={true}
+                  />
+                  <div className={classes.btn_container}>
+                    <button
+                      className={classes.btn}
+                      onClick={() => increment(item)}
+                    >
+                      <IoIosArrowUp size={20} />
+                    </button>
+                    <span>{item.amount}</span>
+                    <button
+                      className={classes.btn}
+                      onClick={() => decrement(item.id)}
+                    >
+                      <IoIosArrowDown size={20} />
+                    </button>
+                  </div>
+                </section>
+              );
+            })
+          )}
+        </div>
+
+       
+      </section>
     </LayOut>
   );
 };
