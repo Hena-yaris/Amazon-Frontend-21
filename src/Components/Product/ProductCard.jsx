09b81f290642cb9660a5c7a3e8c2231ebff613f0
Hena-yaris <meraey21@gmail.com>
2024-02-27 21:37:14 -0800

    category part

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -1,37 +1,35 @@
 import Rating from '@mui/material/Rating'
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
 import classes from './Product.module.css'
+import { Link } from 'react-router-dom';
 
 
 function ProductCard({product}) {
     const { image, title, id, rating, price,description  } = product;
 
   return (
-    <div  className={classes.card__container}>
-        <a href="">
-            <img src={image} alt="" className={classes.img_container}/>
-        </a>
-        <div>
-            <h3>{title}</h3>
-          
-            <div className={classes.rating}>
-                 {/* rating */}
-                 <Rating value={rating?.rate} precision={0.1}/>
-                  {/*count  */}
-                  <small>{rating?.count}</small>
+    <div className={classes.card__container}>
+      <Link to={`/products/${id}`}>
+        <img src={image} alt="" className={classes.img_container} />
+      </Link>
+      <div>
+        <h3>{title}</h3>
 
-            </div>
-            <div>
-                {/* price */}
-                <CurrencyFormat amount={price}/>
-            </div>
-      
-        <button className={classes.button} >
-        add to cart
-        </button>
+        <div className={classes.rating}>
+          {/* rating */}
+          <Rating value={rating?.rate} precision={0.1} />
+          {/*count  */}
+          <small>{rating?.count}</small>
         </div>
+        <div>
+          {/* price */}
+          <CurrencyFormat amount={price} />
+        </div>
+
+        <button className={classes.button}>add to cart</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProductCard;
--- src/Router.jsx
+++ src/Router.jsx
@@ -6,7 +6,7 @@ import Payment from "./Pages/Payment/Payment";
 import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
-// import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 
 function Routing() {
   return (
@@ -17,8 +17,8 @@ function Routing() {
         <Route path="/payments" element={<Payment />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/category" element={<Results />} />
-        {/* <Route path="/products/:productId" element={<ProductDetail />} /> */}
+        <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
       </Routes>
     </Router>
   );
